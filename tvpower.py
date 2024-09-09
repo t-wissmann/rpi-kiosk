dbbceced1f7f9c8c249db922097cdf278c992c07
Thorsten Wißmann <edu@thorsten-wissmann.de>
2024-09-09 22:08:27 +0200

    Fixes in tvpower.py

--- tvpower.py
+++ tvpower.py
@@ -3,12 +3,22 @@ import sys
 import argparse
 import subprocess
 
+
+def debug(mesg):
+    print(mesg, file=sys.stderr)
+
+
+def run_cmd(command):
+    debug(':: ' + ' '.join(command))
+    subprocess.run(command)
+
+
 def main():
     """
     Switch the connected TV screens on and off. Requires
     screens connected via hdmi that can be controlled via cec (using cec-ctl).
     """
-    parser = argparse.ArgumentParser(description=main.__help__)
+    parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument('--on', default=False, action='store_true',
                         help='switch hdmi tv screen on')
     parser.add_argument('--off', default=False, action='store_true',
@@ -24,10 +34,13 @@ def main():
     # cec-ctl -d0 --tv -S  # <- configure the adapter
     cec_ctl = ['cec-ctl', '-d0']
     if args.on:
-        subprocess.run(cec_ctl + ['-t0', '--image-view-on'])
+        run_cmd(cec_ctl + ['-t0', '--image-view-on'])
     if args.off:
-        subprocess.run(cec_ctl + ['--tv', '-S'])
-        subprocess.run(cec_ctl + ['-t0', '--standby'])
+        run_cmd(cec_ctl + ['--tv', '-S'])
+        run_cmd(cec_ctl + ['-t0', '--standby'])
+    if not args.on and not args.off:
+        debug('Warning: neither --off nor --on supplied.')
+
 
 if __name__ == "__main__":
     sys.exit(main())
